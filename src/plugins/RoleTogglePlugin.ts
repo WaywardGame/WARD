fe4d30b6b59d973784fb8109b4f0b0bff77589ee
Mackenzie McClane <yuudaari@gmail.com>
2019-05-07 07:20:36 -0500

    Improve role toggle messages, use manage_roles permission instead of "mod" role

--- src/plugins/RoleTogglePlugin.ts
+++ src/plugins/RoleTogglePlugin.ts
@@ -1,20 +1,15 @@
-import { Collection, Message, Role } from "discord.js";
-
+import { Collection, Message, Permissions } from "discord.js";
 import { Plugin } from "../core/Plugin";
+
 
 export interface IRoleTogglePluginConfig {
 	toggleableRoles: { [key: string]: string[] };
 }
 
 export class RoleTogglePlugin extends Plugin<IRoleTogglePluginConfig> {
-	private roleMod: Role;
 
 	public getDefaultId () {
 		return "roleToggle";
-	}
-
-	public onStart () {
-		this.roleMod = this.guild.roles.find(role => role.name === "mod");
 	}
 
 	public onCommand (message: Message, command: string, ...args: string[]) {
@@ -46,7 +41,7 @@ export class RoleTogglePlugin extends Plugin<IRoleTogglePluginConfig> {
 
 		let toggleMember = message.member;
 		if (queryMember) {
-			if (!message.member.roles.has(this.roleMod.id)) {
+			if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_ROLES)) {
 				this.reply(message, "only mods can toggle the roles of other members.");
 				return;
 			}
@@ -68,12 +63,12 @@ export class RoleTogglePlugin extends Plugin<IRoleTogglePluginConfig> {
 		if (toggleMember.roles.has(role.id)) {
 			toggleMember.removeRole(role);
 			this.log(`Removed role ${role.name} from ${toggleMember.displayName}`);
-			this.reply(message, `Removed role ${role.name} from ${toggleMember.displayName}`);
+			this.reply(message, toggleMember === message.member ? `you no longer have the role "${role.name}".` : `Removed role "${role.name}" from ${toggleMember.displayName}.`);
 
 		} else {
 			toggleMember.addRole(role);
 			this.log(`Added role ${role.name} to ${toggleMember.displayName}`);
-			this.reply(message, `Added role ${role.name} to ${toggleMember.displayName}`);
+			this.reply(message, toggleMember === message.member ? `you have been given the role "${role.name}".` : `Added role "${role.name}" to ${toggleMember.displayName}.`);
 		}
 	}
 }
