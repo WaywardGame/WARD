8c53d7b5804188a01ac19b7229fd3760aca19146
Mackenzie McClane <yuudaari@gmail.com>
2019-05-11 11:31:25 -0500

    Accept type args in ExternalPlugin

--- src/core/ExternalPlugin.ts
+++ src/core/ExternalPlugin.ts
@@ -4,7 +4,7 @@ export interface ExternalPluginEntryPoint {
 	initialize (cls: typeof ExternalPlugin): ExternalPlugin;
 }
 
-export default abstract class ExternalPlugin extends Plugin {
+export default abstract class ExternalPlugin<Config extends {} = {}, DataIndex extends string | number = string | number> extends Plugin<Config, DataIndex> {
 	protected getDataPath () {
 		return `data/${this.guild.id}/external/${this.getId()}.json`;
 	}
