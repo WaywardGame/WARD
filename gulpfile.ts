065fef3b68fcc2c6c8c7364c2782254123974144
Mackenzie McClane <yuudaari@gmail.com>
2020-05-16 21:10:45 +1000

    Use even-more-min for mocha

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,4 +1,4 @@
-import Task, { remove, Pipe } from "./gulp/Task";
+import Task, { Pipe, remove } from "./gulp/Task";
 import TypescriptWatch from "./gulp/TypescriptWatch";
 import mocha = require("gulp-mocha");
 
@@ -7,7 +7,7 @@ import mocha = require("gulp-mocha");
 //
 
 Task.create("mocha", Pipe.create("out/tests/Main.js", { read: false })
-	.pipe(() => mocha({ reporter: "min" }))
+	.pipe(() => mocha({ reporter: "even-more-min" }))
 	.on("error", () => process.exitCode = 1));
 
 new Task("compile-test", remove("out"))
