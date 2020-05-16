04b6e24fd43c7277bcabce1130cfd4b258010afb
Mackenzie McClane <yuudaari@gmail.com>
2020-05-16 21:09:57 +1000

    Re-enable banning spam acct functionality
    (It's been being tested for months so like it's probably good now yeah?)

--- src/plugins/SpamPlugin.ts
+++ src/plugins/SpamPlugin.ts
@@ -1,28 +1,49 @@
 
-import { Plugin } from "../core/Plugin";
 import { Message } from "discord.js";
+import { Plugin } from "../core/Plugin";
 import { sleep } from "../util/Async";
+import { getTime, TimeUnit } from "../util/Time";
 
 const discordURLRegex = /\bdiscord\.gg\/[A-Za-z0-9]{4,}\b/;
 
-export class SpamPlugin extends Plugin<{}> {
+export interface ISpamPluginConfig {
+	banIfLeaveWithin?: string | [TimeUnit, number] | false;
+}
+
+export class SpamPlugin extends Plugin<ISpamPluginConfig> {
 	public getDefaultId () {
 		return "spam";
 	}
 
 	public async onMessage (message: Message) {
-		if (message.type === "GUILD_MEMBER_JOIN") {
-			if (discordURLRegex.test(message.author.username)) {
-				message.delete();
-				this.guild.ban(message.author, { reason: "Bad username" });
-				this.log(`Banned user ${message.author.username}. Reason: Bad username.`);
+		if (message.type !== "GUILD_MEMBER_JOIN")
+			return;
 
-			} else if ((await sleep(5000)) || !this.guild.members.get(message.author.id)) {
-				message.delete();
-				this.guild.ban(message.author, { reason: "Suspicious activity" });
-				this.log(`Banned user ${message.author.username}. Reason: Suspicious activity.`);
-			}
+		if (discordURLRegex.test(message.author.username)) {
+			message.delete();
+			this.guild.ban(message.author, { reason: "Bad username" });
+			this.logger.warning(`Banned user ${message.author.username}. Reason: Bad username.`);
+			return;
 		}
+
+		if (this.config.banIfLeaveWithin === false)
+			return;
+
+		await sleep(getTime(this.config.banIfLeaveWithin) || 5000);
+
+		await this.guild.fetchMembers();
+
+		if (!this.guild.members.get(message.author.id)) {
+			message.delete();
+			this.guild.ban(message.author, { reason: "Suspicious activity" });
+			this.logger.warning(`Banned user ${message.author.username}. Reason: Suspicious activity.`);
+		}
+	}
+
+	public getDefaultConfig () {
+		return {
+			banIfLeaveWithin: "5 seconds",
+		};
 	}
 
 }
